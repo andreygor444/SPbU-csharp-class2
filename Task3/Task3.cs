a772aefb9b2a158237326278bc03eed87fb4de6e
Andrey <andreygor444@gmail.com>
2022-09-10 16:27:14 +0700

    Доделал Task3

--- Task3/Task3.cs
+++ Task3/Task3.cs
@@ -12,36 +12,32 @@ namespace Task3
 /*
  * Задание 3.1. Проверить, содержит ли заданная строка только цифры?
  */
-        internal static bool AllDigits(string s) => new Regex("\d").IsMatch(s);
+        internal static bool AllDigits(string s) => new Regex(@"^\d*$").IsMatch(s);
 
 /*
  * Задание 3.2. Проверить, содержит ли заданная строка подстроку, состоящую
  * из букв abc в указанном порядке, но в произвольном регистре?
  */
-        internal static bool ContainsABC(string s) => new Regex("WRITE_ME", RegexOptions.None).IsMatch(s);
+        internal static bool ContainsABC(string s) => new Regex("[aA][bB][cC]", RegexOptions.None).IsMatch(s);
 
 /*
  * Задание 3.3. Найти первое вхождение подстроки, состоящей только из цифр,
  * и вернуть её в качестве результата. Вернуть пустую строку, если вхождения нет.
  */
-        internal static string FindDigitalSubstring(string s)
-        {
-            throw new NotImplementedException();
-        }
+        internal static string FindDigitalSubstring(string s) => new Regex(@"\d+").Match(s).ToString();
 
 /*
  * Задание 3.4. Заменить все вхождения подстрок строки S, состоящих только из цифр,
  * на заданную строку S1.
  */
-        internal static string HideDigits(string s, string s1)
-        {
-            throw new NotImplementedException();
-        }
+        internal static string HideDigits(string s, string s1) => new Regex(@"\d+").Replace(s, s1);
 
-        public static void Main(string[] args)
+            public static void Main(string[] args)
         {
-            throw new NotImplementedException(
-                "Вызовите здесь все перечисленные в классе функции, как это сделано в предыдущих заданиях");
+            AllDigits("23425");
+            ContainsABC("dfbvkhABCvjkndf");
+            FindDigitalSubstring("dfvkj324jkfb");
+            HideDigits("dfkldvb23434cvn jk5t43bhb", "dfvs");
         }
     }
 }
